1c636fce15b8cdfe5ffef7d2ea674ac592cf13bb
ChunkyPanda <neil.malhotra0@gmail.com>
2015-12-11 22:32:39 -0600

    2nd chance

--- randi.s
+++ randi.s
@@ -2,21 +2,30 @@
 .func main
    
 main:
-    MOV R0, #0              @ initialze index variable
+    MOV R0, #0
+    MOV R5, #0
+    MOV R4, #0
 writeloop:
-    CMP R0, #100            @ check to see if we are done iterating
-    BEQ writedone           @ exit loop if done
-    LDR R1, =a              @ get address of a
-    LSL R2, R0, #2          @ multiply index*4 to get array offset
-    ADD R2, R1, R2
-    BL _scanf          @ R2 now has the element address
-    MOV R2, R0            @ write the address of a[i] to a[i]
-    ADD R0, R0, #1          @ increment index
+    CMP R0, #10 
+    BEQ writedone
+    PUSH {R0}
+    BL _scanf
+    CMP R5,R0
+    MOVLT R5,R0
+    CMP R4,R0
+    MOVGT R4,R0
+    MOV R7,R0
+    POP {R0}
+    LDR R1, =a
+    LSL R2,R0,#2
+    ADD R2,R1,R2
+    STR R7,[R2]
+    ADD R0,R0,#1
     B   writeloop           @ branch to next loop iteration
 writedone:
     MOV R0, #0              @ initialze index variable
 readloop:
-    CMP R0, #100            @ check to see if we are done iterating
+    CMP R0, #10             @ check to see if we are done iterating
     BEQ readdone            @ exit loop if done
     LDR R1, =a              @ get address of a
     LSL R2, R0, #2          @ multiply index*4 to get array offset
@@ -34,8 +43,11 @@ readloop:
     ADD R0, R0, #1          @ increment index
     B   readloop            @ branch to next loop iteration
 readdone:
+    BL min
     B _exit                 @ exit if done
-    
+min:
+   MOV R1,R4
+   BL _printmin    
 _exit:  
     MOV R7, #4              @ write syscall, 4
     MOV R0, #1              @ output stream to monitor, 1
@@ -44,7 +56,11 @@ _exit:
     SWI 0                   @ execute syscall
     MOV R7, #1              @ terminate syscall, 1
     SWI 0                   @ execute syscall
-       
+_printmin:
+    PUSH {LR}
+    LDR R0, =print_min
+    BL printf
+    POP {PC}       
 _printf:
     PUSH {LR}               @ store the return address
     LDR R0, =printf_str     @ R0 contains formatted string address
@@ -53,10 +69,10 @@ _printf:
 _scanf:
     MOV R4, LR              @ store LR since scanf call overwrites
     SUB SP, SP, #4          @ make room on stack
-    LDR R3, =format_str     @ R0 contains address of format string
+    LDR R0, =format_str     @ R0 contains address of format string
     MOV R1, SP              @ move SP to R1 to store entry on stack
     BL scanf                @ call scanf
-    LDR R3, [SP]            @ load value at SP into R0
+    LDR R0, [SP]            @ load value at SP into R0
     ADD SP, SP, #4          @ restore the stack pointer
     MOV PC, R4              @ retu
    
@@ -67,3 +83,4 @@ a:              .skip       40
 printf_str:     .asciz      "a[%d] = %d\n"
 exit_str:       .ascii      "Terminating program.\n"
 format_str:      .ascii      "%d"
+print_min:       .ascii       "%d"
